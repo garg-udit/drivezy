154066ea5e504c8dc7478d97dda1f0515a5b6f27
Udit Garg <uditgarg540@gmail.com>
2025-08-19 22:05:10 +0530

    test file uploaded

---
+++ backend/src/test/java/com/drivezy/drivezyBackend/DrivezyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drivezy.drivezyBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivezyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
